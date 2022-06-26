71332afcf691bd7116508e2f2d2f0bffc2c7b0c7
Michael Rissi <michael.rissi@gmail.com>
2022-06-26 16:26:06 +0200

    distinguish between ankunft and abfahrts for the delays.

--- data_cleaner.py
+++ data_cleaner.py
@@ -60,8 +60,8 @@ def select_trains(df):
     return df
 
 # filter data where the an/ab prognose is "REAL"
-def real_prognose_filter(df):
-    """Only select entries from the istdaten df where the AN_PROGNOSE_STATUS and the AB_PROGNOSE_STATUS is REAL.
+def real_prognose_filter_abfahrt(df):
+    """Only select entries from the istdaten df where the  AB_PROGNOSE_STATUS is REAL.
 
     Parameters
     ----------
@@ -75,9 +75,26 @@ def real_prognose_filter(df):
         The istdaten DataFrame REAL delays
     """
 
-    df = df[(df['AN_PROGNOSE_STATUS'] == 'REAL') & (df['AB_PROGNOSE_STATUS'] == 'REAL')]
+    df = df[df['AB_PROGNOSE_STATUS'] == 'REAL']
     return df
 
+def real_prognose_filter_ankunft(df):
+    """Only select entries from the istdaten df where the  AN_PROGNOSE_STATUS is REAL.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        the istdaten DataFrame
+
+
+    Returns
+    -------
+    pd.DataFrame
+        The istdaten DataFrame REAL delays
+    """
+
+    df = df[df['AN_PROGNOSE_STATUS'] == 'REAL']
+    return df
 
 def convert_to_datetimes(df):
     """Convert string datetimes in the istdaten df to datetimes
@@ -106,8 +123,8 @@ def convert_to_datetimes(df):
     return df
 
 # filter bad data. i.e. nans
-def bad_data_filter(df):
-    """Filter rows where AN_PROGNOSE, AB_PROGNOSE, ABFAHRTSZEIT or ANKUNFTSZEIT is not nan.
+def bad_data_filter_abfahrt(df):
+    """Filter rows where  AB_PROGNOSE aor ABFAHRTSZEIT is not nan.
 
     Parameters
     ----------
@@ -121,13 +138,30 @@ def bad_data_filter(df):
         The istdaten DataFrame without NaNs.
     """
 
-    df = df[(df['AN_PROGNOSE'].notna()) & (df['AB_PROGNOSE'].notna()) & (df['ABFAHRTSZEIT'].notna()) & (
-        df['ANKUNFTSZEIT'].notna())]
+    df = df[(df['AB_PROGNOSE'].notna()) & (df['ABFAHRTSZEIT'].notna())]
+    return df
+
+def bad_data_filter_abfahrt(df):
+    """Filter rows where  AN_PROGNOSE aor ANKUNFTSZEIT is not nan.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        the istdaten DataFrame
+
+
+    Returns
+    -------
+    pd.DataFrame
+        The istdaten DataFrame without NaNs.
+    """
+
+    df = df[(df['AN_PROGNOSE'].notna()) & (df['ANKUNFTSZEIT'].notna())]
     return df
 
 ## calculate the delays. We calculate those in seconds. Reason: timedeltas suck.
-def calculate_delay(df):
-    """Add a column to the istdaten df corresponding to the arrival and departure delays, measured in seconds.
+def calculate_delay_abfahrt(df):
+    """Add a column to the istdaten df corresponding to  departure delays, measured in seconds.
 
     Parameters
     ----------
@@ -142,11 +176,28 @@ def calculate_delay(df):
     """
 
     df['ABFAHRTSVERSPAETUNG_s'] = (df['AB_PROGNOSE'] - df['ABFAHRTSZEIT']) / pd.Timedelta(seconds=1)
-    df['ANKUNFTSVERSPAETUNG_s'] = (df['AN_PROGNOSE'] - df['ANKUNFTSZEIT']) / pd.Timedelta(seconds=1)
     return df
 
+    def calculate_delay_ankunft(df):
+        """Add a column to the istdaten df corresponding to  arrival delays, measured in seconds.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            the istdaten DataFrame
+
+
+        Returns
+        -------
+        pd.DataFrame
+            The istdaten DataFrame with the new delay columns.
+        """
+
+        df['ANKUNFTSVERSPAETUNG_s'] = (df['AN_PROGNOSE'] - df['ANKUNFTSZEIT']) / pd.Timedelta(seconds=1)
+        return df
+
 # no train in switzerland has a delay longer than 1 hour
-def remove_crazy_delays(df):
+def remove_crazy_delays_abfahrt(df):
     """Remove rows with a delay larger than 1 hour. No train in Switzerland has such a large delay!
 
     Parameters
@@ -161,10 +212,29 @@ def remove_crazy_delays(df):
         The istdaten DataFrame without crazy delays.
     """
 
-    df = df[(df['ANKUNFTSVERSPAETUNG_s'] < timedelta(hours=1) / timedelta(seconds=1)) & (
+    df = df[(df['ABFAHRTSVERSPAETUNG_s'] > timedelta(hours=-1) / timedelta(seconds=1)) & (
                 df['ABFAHRTSVERSPAETUNG_s'] < timedelta(hours=1) / timedelta(seconds=1))]
+
+    return df
+
+def remove_crazy_delays_ankunft(df):
+    """Remove rows with a delay larger than 1 hour. No train in Switzerland has such a large delay!
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        the istdaten DataFrame
+
+
+    Returns
+    -------
+    pd.DataFrame
+        The istdaten DataFrame without crazy delays.
+    """
+
     df = df[(df['ANKUNFTSVERSPAETUNG_s'] > timedelta(hours=-1) / timedelta(seconds=1)) & (
-                df['ABFAHRTSVERSPAETUNG_s'] > timedelta(hours=-1) / timedelta(seconds=1))]
+                df['ANKUNFTSVERSPAETUNG_s'] < timedelta(hours=1) / timedelta(seconds=1))]
+
     return df
 
 
@@ -186,9 +256,9 @@ def clean_data(df):
     df = select_trains(df)
     df = remove_unnecessary_columns(df)
     df = convert_to_datetimes(df)
-    df = real_prognose_filter(df)
-    df = bad_data_filter(df)
-    df = calculate_delay(df)
+    df = real_prognose_filter_abfahrt(df)
+    df = bad_data_filter_abfahrt(df)
+    df = calculate_delay_abfahrt(df)
 
     return df
 
